ce07383cdd97255dc199266a52edc177244fb5dd
Ammar Izhar <ammar110izhar@gmail.com>
2025-07-12 18:01:28 +0500

    Contact us Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import emailjs from 'emailjs-com';
+import { HttpErrorResponse } from '@angular/common/http';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-//  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+  public model: any = {};
+  constructor() { }
+
+  sendEmail(form: NgForm) {
+    debugger;
+    if (this.model != null)
+    {
+      emailjs.send('service_dcjzjb9', 'template_vlfboma', this.model, 'Xob0I7u0oLyPnmcRJ')
+        .then(response => {
+          this.model.reset();
+        })
+        .catch((error: HttpErrorResponse) => {
+          if (error.error && error.error.errors) {
+
+          }
+        })
+    } else {
+
+    }
+  }
 }
